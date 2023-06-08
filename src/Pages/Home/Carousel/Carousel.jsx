f36d161f84cde7c98680708586d8b74d25e96544
Md Jamil Akter <jamilakterup@gmail.com>
2023-06-09 05:05:50 +0600

    banner done

--- src/Pages/Home/Carousel/Carousel.jsx
+++ src/Pages/Home/Carousel/Carousel.jsx
@@ -7,9 +7,12 @@ import img5 from '../../../assets/img5.jpg';
 const Carousel = () => {
     return (
         <div className="carousel w-full">
-            <div id="slide1" className="flex-col-reverse md:flex-row carousel-item relative md:h-full h-[50vh] w-full bg-green-100">
-                <div className='w-full md:w-1/2 md:ps-24 flex items-center justify-center md:mb-0 md:mt-0 mt-4 mb-10'>
-                    <h1 className="md:text-5xl font-bold"> Let the music of summer camp be the soundtrack <br /> of your adventure!</h1>
+            <div id="slide1" className="flex-col-reverse md:flex-row carousel-item relative md:h-full h-[55vh] w-full bg-green-300">
+                <div className='w-full md:w-1/2 md:ps-24 flex items-center justify-center md:mb-0 md:mt-0 my-4'>
+                    <div>
+                        <h1 className="md:text-5xl font-bold"> Let the music of summer camp be the soundtrack <br /> of your adventure!</h1>
+                        <button className="btn mt-6">Get Start</button>
+                    </div>
                 </div>
                 <img src={img2} className="w-full md:w-1/2 md:h-[415px] h-[240px] md:mt-24" />
 
@@ -18,33 +21,42 @@ const Carousel = () => {
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide2" className="flex-col-reverse md:flex-row carousel-item relative w-full bg-green-100">
+            <div id="slide2" className="flex-col-reverse md:flex-row carousel-item relative md:h-full h-[55vh] w-full bg-green-300">
                 <div className='w-full md:w-1/2 md:ps-24 flex items-center justify-center md:mb-0 md:mt-0 mt-4  mb-12'>
-                    <h1 className="md:text-5xl font-bold"> Let the music of summer camp be the soundtrack <br /> of your adventure!</h1>
+                    <div>
+                        <h1 className="md:text-5xl font-bold"> Summer camp is the stage where your musical <br /> dreams come alive</h1>
+                        <button className="btn mt-6">Get Start</button>
+                    </div>
                 </div>
-                <img src={img2} className="w-full md:w-1/2 md:h-[415px] h-[240px] md:mt-24" />
+                <img src={img3} className="w-full md:w-1/2 md:h-[415px] h-[240px] md:mt-24" />
 
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide3" className="flex-col-reverse md:flex-row carousel-item relative w-full bg-green-100">
+            <div id="slide3" className="flex-col-reverse md:flex-row carousel-item relative md:h-full h-[55vh]  w-full bg-green-300">
                 <div className='w-full md:w-1/2 md:ps-24 flex items-center justify-center md:mb-0 md:mt-0 mt-4  mb-12'>
-                    <h1 className="md:text-5xl font-bold"> Let the music of summer camp be the soundtrack <br /> of your adventure!</h1>
+                    <div>
+                        <h1 className="md:text-5xl font-bold"> {"Let's"} jump to the music ocean</h1>
+                        <button className="btn mt-6">Get Start</button>
+                    </div>
                 </div>
-                <img src={img2} className="w-full md:w-1/2 md:h-[415px] h-[240px] md:mt-24" />
+                <img src={img1} className="w-full md:w-1/2 md:h-[415px] h-[240px] md:mt-24" />
 
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide4" className="flex-col-reverse md:flex-row carousel-item relative w-full bg-green-100">
+            <div id="slide4" className="flex-col-reverse md:flex-row carousel-item relative md:h-full h-[55vh]  w-full bg-green-300">
                 <div className='w-full md:w-1/2 md:ps-24 flex items-center justify-center md:mb-0 md:mt-0 mt-4  mb-12'>
-                    <h1 className="md:text-5xl font-bold"> Let the music of summer camp be the soundtrack <br /> of your adventure!</h1>
+                    <div>
+                        <h1 className="md:text-5xl font-bold"> Let the music of summer camp be the soundtrack <br /> of your adventure!</h1>
+                        <button className="btn mt-6">Get Start</button>
+                    </div>
                 </div>
-                <img src={img2} className="w-full md:w-1/2 md:h-[415px] h-[240px] md:mt-24" />
+                <img src={img5} className="w-full md:w-1/2 md:h-[415px] h-[240px] md:mt-24" />
 
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide3" className="btn btn-circle">❮</a>
