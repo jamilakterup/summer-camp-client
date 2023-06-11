7a778f8a6e3a5202b124b3aa6cd5e88d3dd3cff1
Md Jamil Akter <jamilakterup@gmail.com>
2023-06-12 00:24:30 +0600

    disabled done

--- src/Pages/Home/PopularClass/PopularClassCard.jsx
+++ src/Pages/Home/PopularClass/PopularClassCard.jsx
@@ -5,36 +5,68 @@ import Typography from '@mui/material/Typography';
 import {Button, CardActionArea, CardActions} from '@mui/material';
 import {useContext} from 'react';
 import {AuthContext} from '../../../components/Providers/AuthProviders';
-import {useNavigate} from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import {toast} from 'react-hot-toast';
 import {useState} from 'react';
+import Swal from 'sweetalert2';
+import {useEffect} from 'react';
 
 const PopularClassCard = ({item}) => {
+    const {classImg, className, instructor, totalSeat, students, price, _id} = item;
+
     const [isClicked, setIsClicked] = useState(false);
     const {user} = useContext(AuthContext);
     const navigate = useNavigate();
+    const location = useLocation();
     const available = parseInt(item.totalSeat) - parseInt(item.students);
     const disabled = available === 0;
 
     const handleAddToFavorite = (item) => {
-        if (user) {
+        if (user && user.email) {
+            const cartItem = {itemId: _id, classImg, className, totalSeat, students, email: user.email}
+            console.log(cartItem);
             fetch('http://localhost:5000/carts', {
                 method: 'POST',
                 headers: {'content-type': 'application/json'},
-                body: JSON.stringify(item)
+                body: JSON.stringify(cartItem)
             })
                 .then(res => res.json())
                 .then(data => {
-                    console.log(data)
                     if (data.insertedId) {
-                        toast.success('Successfully toasted!')
+                        console.log(data.insertedId)
+                        toast.success('Successfully added to the favorite!')
                         setIsClicked(true);
                     }
                 })
         } else {
-            navigate('/login')
+            Swal.fire({
+                title: 'Please Login First',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', {state: {from: location}});
+                }
+            })
         }
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/carts')
+            .then(res => res.json())
+            .then(data => {
+                data.find(d => {
+                    if (d.itemId === _id) {
+                        return setIsClicked(true);
+                    }
+                });
+
+            })
+    }, [])
 
     return (
         <div>
@@ -46,23 +78,23 @@ const PopularClassCard = ({item}) => {
                         component="img"
                         height="140"
                         style={{height: '300px'}}
-                        image={item.classImg}
+                        image={classImg}
                         alt="green iguana"
                     />
                     <CardContent disabled>
                         <Typography gutterBottom variant="h5" component="div">
-                            {item.class}
+                            {className}
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-                            <h4 className='text-xl'>Instructor: {item.instructor}</h4>
+                            <h4 className='text-xl'>Instructor: {instructor}</h4>
                             <div className="flex justify-between mt-2 me-3">
                                 <div>
-                                    <p>Total seat: {item.totalSeat}</p>
-                                    <p>Students: {item.students}</p>
+                                    <p>Total seat: {totalSeat}</p>
+                                    <p>Students: {students}</p>
                                 </div>
                                 <div>
                                     <p>Available seats: {available}</p>
-                                    <p>Price: ${item.price}</p>
+                                    <p>Price: ${price}</p>
                                 </div>
                             </div>
                         </Typography>
