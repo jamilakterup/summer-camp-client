1a91b2dd80317f3435980b8271c56ddb318404ba
Md Jamil Akter <jamilakterup@gmail.com>
2023-06-11 20:34:20 +0600

    disabled on button click

--- src/Pages/Home/PopularClass/PopularClassCard.jsx
+++ src/Pages/Home/PopularClass/PopularClassCard.jsx
@@ -3,10 +3,38 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import {Button, CardActionArea, CardActions} from '@mui/material';
+import {useContext} from 'react';
+import {AuthContext} from '../../../components/Providers/AuthProviders';
+import {useNavigate} from 'react-router-dom';
+import {toast} from 'react-hot-toast';
+import {useState} from 'react';
 
 const PopularClassCard = ({item}) => {
+    const [isClicked, setIsClicked] = useState(false);
+    const {user} = useContext(AuthContext);
+    const navigate = useNavigate();
     const available = parseInt(item.totalSeat) - parseInt(item.students);
     const disabled = available === 0;
+
+    const handleAddToFavorite = (item) => {
+        if (user) {
+            fetch('http://localhost:5000/carts', {
+                method: 'POST',
+                headers: {'content-type': 'application/json'},
+                body: JSON.stringify(item)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.insertedId) {
+                        toast.success('Successfully toasted!')
+                        setIsClicked(true);
+                    }
+                })
+        } else {
+            navigate('/login')
+        }
+    }
 
     return (
         <div>
@@ -40,7 +68,7 @@ const PopularClassCard = ({item}) => {
                         </Typography>
                     </CardContent>
                     <CardActions>
-                        <Button style={{backgroundColor: '#f0f0f0', color: '#000', marginBottom: '10px'}} variant="contained">Contained</Button>
+                        <Button onClick={() => handleAddToFavorite(item)} disabled={isClicked} style={{backgroundColor: '#f0f0f0', color: '#000', marginBottom: '10px'}} variant="contained">add to favorite</Button>
                     </CardActions>
                 </CardActionArea>
             </Card>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css';
 import {RouterProvider} from "react-router-dom";
 import router from './Routes/Routes';
 import AuthProviders from './components/Providers/AuthProviders';
+import {Toaster} from 'react-hot-toast';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProviders>
+      <Toaster position="top-center" reverseOrder={false} />
       <RouterProvider router={router} />
     </AuthProviders>
   </React.StrictMode>,
